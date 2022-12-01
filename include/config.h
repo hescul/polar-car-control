d5b57acf98af82873dab2bc78ab8854434b69197
hescul <ndminh1101@gmail.com>
2022-12-01 15:13:20 +0700

    rewire pin connection
    Signed-off-by: hescul <ndminh1101@gmail.com>

--- include/config.h
+++ include/config.h
@@ -15,14 +15,14 @@ constexpr uint8_t ENCR_B = 5;
 
 constexpr long BLE_BR = 9600;
 constexpr uint8_t BLE_TX = 13;  // software serial
-constexpr uint8_t BLE_RX = 12;  // software serial
+constexpr uint8_t BLE_RX = 10;  // redundant, but put here for completeness
 
-constexpr uint8_t PWML = 10;    // PWM
-constexpr uint8_t INL_1 = 7;
-constexpr uint8_t INL_2 = 6;
+constexpr uint8_t PWML = 11;    // PWM
+constexpr uint8_t INL_1 = 8;
+constexpr uint8_t INL_2 = 12;
 
-constexpr uint8_t PWMR = 11;    // PWM
-constexpr uint8_t INR_1 = 9;
-constexpr uint8_t INR_2 = 8;
+constexpr uint8_t PWMR = 9;    // PWM
+constexpr uint8_t INR_1 = 6;
+constexpr uint8_t INR_2 = 7;
 
 #endif //PID_CONFIG_H
